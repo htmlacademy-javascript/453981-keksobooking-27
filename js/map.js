fb2f49a1f7edd67d0fcbe6648e15483c8a2e095d
Victoria Yakovleva <vikyss@mail.ru>
2022-11-18 13:43:16 +0200

    feat: fix fetch data from server

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ import { setPageState } from './utils.js';
 
 const TOKYO_LAT = 35.68950;
 const TOKYO_LNG = 139.69171;
-const ZOOM = 12;
+const ZOOM = 10;
 const MAX_ZOOM = 30;
 const TILE_LAYER_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const TILE_LAYER_ATTRIBUTION = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
@@ -55,33 +55,31 @@ const onMarkerMove = (evt) => {
 
 map.on('load', () => {
   setPageState(true);
-  getOffers()
-    .then((data) => data.json())
-    .then((data) => {
-      data.forEach((offer) => {
-        L.marker({
-          lat: offer.location.lat,
-          lng: offer.location.lng,
-        }, {
-          icon: pinIcon,
-        })
-          .addTo(map)
-          .bindPopup(createCard(offer));
-      });
-    })
-    .catch(() => {
-      const requestError = requestErrorTemplate.cloneNode(true);
-      const button = requestError.querySelector('.error__button');
-
-      document.body.appendChild(requestError);
-      button.addEventListener('click', () => {
-        requestError.remove();
-      });
+  getOffers(async (data) => {
+    const offers = await data.json();
+    offers.forEach((offer) => {
+      L.marker({
+        lat: offer.location.lat,
+        lng: offer.location.lng,
+      }, {
+        icon: pinIcon,
+      })
+        .addTo(map)
+        .bindPopup(createCard(offer));
     });
+  }, () => {
+    const requestError = requestErrorTemplate.cloneNode(true);
+    const button = requestError.querySelector('.error__button');
+
+    document.body.appendChild(requestError);
+    button.addEventListener('click', () => {
+      requestError.remove();
+    });
+  });
 }).setView([TOKYO_LAT, TOKYO_LNG], ZOOM);
 
 export function resetMapState() {
-  map.setView([TOKYO_LAT, TOKYO_LNG]);
+  map.setView([TOKYO_LAT, TOKYO_LNG], ZOOM);
   map.closePopup();
   mainMarker.setLatLng([TOKYO_LAT, TOKYO_LNG]);
 }
